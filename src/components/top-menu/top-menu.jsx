900e1127b10bf5fa872df450797ac25e54ae2b0c
chelm <chelmicki.p@gmail.com>
2023-09-20 18:41:39 +0200

    update top menu label

--- src/components/top-menu/top-menu.jsx
+++ src/components/top-menu/top-menu.jsx
@@ -5,7 +5,7 @@ import { ROUTE_ABOUT_ME, ROUTE_INDEX } from '@/config/routes';
 
 function TopMenu({ className = "" }) {
     return <nav className={classNames(className)}>
-        <MenuTab path={ROUTE_INDEX}>Readme</MenuTab>
+        <MenuTab path={ROUTE_INDEX}>README.md</MenuTab>
         <MenuTab path={ROUTE_ABOUT_ME}>CV generator</MenuTab>
     </nav>
 }
