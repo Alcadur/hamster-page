70cbdef9d2da82d8ed6a65d84b5a3f6ca2b0ea2b
chelm <chelmicki.p@gmail.com>
2023-09-20 18:47:54 +0200

    update left menu content based on path

--- src/components/left-menu/left-menu.jsx
+++ src/components/left-menu/left-menu.jsx
@@ -1,8 +1,25 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { ROUTE_ABOUT_ME, ROUTE_INDEX } from '@/config/routes';
+import { AboutMeLeftMenu } from '@/components/about-me/about-me-left-menu';
+
+/**
+ * @returns {JSX.Element<LeftMenuContentTemplate>}
+ * @constructor
+ */
 function LeftMenu() {
+    const pathname = usePathname();
+
+
+    if (pathname === ROUTE_INDEX) {
+        return <AboutMeLeftMenu />;
+    }
+
     return (
-        <nav>
-            Left menu
-        </nav>
+        <section>
+
+        </section>
     );
 }
 
