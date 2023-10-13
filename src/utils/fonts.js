6a6330300a6ecb65a0398d12f813e2a5211db27d
chelm <chelmicki.p@gmail.com>
2023-10-13 19:17:16 +0200

    add new layout fonts utils and colors

---
+++ src/utils/fonts.js
@@ -0,0 +1,4 @@
+import { Itim, Jost } from 'next/font/google';
+
+export const headerFont = Itim({ weight: '400', subsets: ['latin'] });
+export const regularTextFont = Jost({ weight: '300', subsets: ['latin'] });
