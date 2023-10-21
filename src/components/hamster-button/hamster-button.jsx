0fd11335dae41d27c61ad37a17e00c193fb53b6e
chelm <chelmicki.p@gmail.com>
2023-10-21 23:37:20 +0200

    update hamster-button to render also as <Link />

--- src/components/hamster-button/hamster-button.jsx
+++ src/components/hamster-button/hamster-button.jsx
@@ -1,18 +1,27 @@
 import style from './hamster-button.module.css';
 import { headerFont } from '@/utils/fonts';
 import classNames from 'classnames';
+import Link from 'next/link';
 
 /**
  * @param {JSX.Element} label
  * @param {boolean} isActive
+ * @param {'link'|'button'} tag
  * @param {string=''} className
+ * @param {string=''} href
  * @returns {JSX.Element}
  * @constructor
  */
-export default function HamsterButton({ children: label, isActive, className = '' }) {
+export default function HamsterButton({ children: label, isActive, tag = 'button', className = '', href }) {
     const classes = classNames(className, style.btn, headerFont.className, {
         [style.btnActive]: isActive
     });
+
+    if (tag === 'link') {
+        return <Link className={classes} href={href}>
+            {label}
+        </Link>
+    }
 
     return <button className={classes}>
         {label}
--- src/components/hamster-button/hamster-button.spec.js
+++ src/components/hamster-button/hamster-button.spec.js
@@ -4,8 +4,8 @@ import { headerFont } from '@/utils/fonts';
 
 describe('HamsterButton', function () {
     describe('render', function () {
-        function prepare({ isActive } = {}) {
-            const { container } = render(<HamsterButton isActive={isActive}>Label</HamsterButton>);
+        function prepare({ isActive, tag } = {}) {
+            const { container } = render(<HamsterButton isActive={isActive} tag={tag} href="/">Label</HamsterButton>);
 
             return { component: container.firstChild };
         }
@@ -27,7 +27,31 @@ describe('HamsterButton', function () {
                 expect(component).toHaveClass('btn');
                 expect(component).toHaveClass(headerFont.className);
                 expect(component).toHaveClass('btnActive');
-            })
+            });
+        });
+
+        describe('when "tag" property is undefined', function () {
+            it('should render component as button', function () {
+                const { component } = prepare({ tag: undefined });
+
+                expect(component.nodeName.toLowerCase()).toEqual('button');
+            });
+        });
+
+        describe('when "tag" property is "link"', function () {
+            it('should render component as a', function () {
+                const { component } = prepare({ tag: 'link' });
+
+                expect(component.nodeName.toLowerCase()).toEqual('a');
+            });
+        });
+
+        describe('when "tag" property is not "a" or "button"', function () {
+            it('should render component as button', function () {
+                const { component } = prepare({ tag: 'label' });
+
+                expect(component.nodeName.toLowerCase()).toEqual('button');
+            });
         });
     });
 });
