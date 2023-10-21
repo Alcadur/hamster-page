cf543403e8dd57c7329f7f26f89ceeb06cb4b1a1
chelm <chelmicki.p@gmail.com>
2023-10-21 23:44:56 +0200

    sync chart animation with % increasing

--- src/components/round-skill-level/round-skill-level-chart.jsx
+++ src/components/round-skill-level/round-skill-level-chart.jsx
@@ -2,16 +2,13 @@
 
 import classNames from 'classnames';
 import style from './round-skill-level.module.css';
-import { useEffect, useRef } from 'react';
-import {
-    ROUND_SKILL_LEVEL_ANIMATION_TIME_IN_MS,
-    useSvgAnimationControl
-} from '@/components/round-skill-level/round-skill-level';
+import { useRef } from 'react';
+import { ROUND_SKILL_LEVEL_ANIMATION_TIME_IN_MS } from '@/components/round-skill-level/round-skill-level';
 
 /**
  * @param {number} value
  * @param {angle} rotateBy
- * @param {function(function): void} animationStartHandlerSetter
+ * @param {boolean} shouldPlayAnimation
  * @returns {JSX.Element}
  * @constructor
  */
@@ -23,7 +20,7 @@ export function RoundSkillLevelChart({ value, rotateBy, shouldPlayAnimation } = 
     const CHART_ANIMATION_BOUNCE_3_VALUE = value - Math.min(value * .06, 3);
 
     if (shouldPlayAnimation) {
-        chartRef.current.classList.add(style.play)
+        chartRef.current.classList.add(style.play);
     }
 
     return <svg data-testid="svg-element" viewBox="0 0 36 36" style={{
--- src/components/round-skill-level/round-skill-level.jsx
+++ src/components/round-skill-level/round-skill-level.jsx
@@ -3,22 +3,24 @@
 import style from './round-skill-level.module.css';
 import { RoundSkillLevelChart } from '@/components/round-skill-level/round-skill-level-chart';
 import { RoundSkillLevelLabel } from '@/components/round-skill-level/round-skill-level-label';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 export const ROUND_SKILL_LEVEL_ANIMATION_TIME_IN_MS = 1000;
 
 /**
  * @param {number} children
  * @param {string} color
- * @param {string} size
  * @param {angle} rotateBy
+ * @param {string} className
  * @returns {JSX.Element}
  * @constructor
  */
-export function RoundSkillLevel({ children, color, size, rotateBy } = {}) {
+export function RoundSkillLevel({ children, color, rotateBy, className } = {}) {
     const numberValue = +children;
     const [currentValue, setCurrentValue] = useState(0);
     const frameTime = ROUND_SKILL_LEVEL_ANIMATION_TIME_IN_MS / numberValue;
+    const containerClassName = classNames(style.container, className);
 
     useIntervalNumberIncrease({
         currentValue,
@@ -27,12 +29,14 @@ export function RoundSkillLevel({ children, color, size, rotateBy } = {}) {
         frameTime
     });
 
-    return <div className={style.container} style={{
+    return <div className={containerClassName} style={{
         '--color': color,
-        '--size': size,
-        '--value': currentValue,
+        '--value': currentValue
     }}>
-        <RoundSkillLevelChart value={currentValue} rotateBy={rotateBy} shouldPlayAnimation={currentValue === numberValue} />
+        <RoundSkillLevelChart value={currentValue} rotateBy={rotateBy}
+                              shouldPlayAnimation={currentValue === numberValue}
+                              parentStyle={{ 'flex-shrink': 0 }}
+        />
         <RoundSkillLevelLabel value={currentValue} />
     </div>;
 }
